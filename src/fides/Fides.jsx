95c8e1de8c0d1ebdff61d6adb90a31854de94186
Sanye Caroline <sanyecg@gmail.com>
2018-09-24 20:35:57 -0300

    changes in footer and menu

---
+++ src/fides/Fides.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import './Fides.css'
+
+export default class Fides extends Component{
+    render(){
+        return(            
+            <div id="fides" className="Fides">FIDES EDUCACIONAL</div>            
+        )
+    }
+}
---
+++ src/publication/Publication.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './Publication.css'
+
+export default class Publication extends Component{
+    render(){
+        return(
+            <div id="publication" className="Publication"> 
+            <div>PUBLICAÇÕES</div>
+            </div>
+        )
+    }
+}
