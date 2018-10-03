519cedaabc75174420d53e2d4edd9da813e08a58
sanyecaroline <sanyecg@gmail.com>
2018-10-03 14:59:34 -0300

    correction app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './App.css';
 
 import Principal from './principal/Principal';
 import Fides from './fides/Fides';
 import Publication from './publication/Publication';
 import Menu from './menu/Menu';
 import Footer from './footer/Footer';
+
 
 class App extends Component {
   render() {
