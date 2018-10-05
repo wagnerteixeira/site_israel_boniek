5fa874c887709fce10aca17e1af5995d5fdefafb
Wagner Bernardes Teixeira <wagnerbernardesteixeira@gmail.com>
2018-10-05 01:52:19 -0300

    Add firebase service to site.

---
+++ frontend/firebase/apis/baseApi.js
@@ -0,0 +1,9 @@
+import { db } from '../firebase';
+
+export const doCreate = (collection, data) => db.collection(collection).add({...data});
+
+export const doGet = (collection) => db.collection(collection).get();
+
+export const doDelete = (collection, id) => db.collection(collection).doc(id).delete();
+
+export const doUpdate = (collection, data) => db.collection(collection).doc(data.id).set(data.data);
---
+++ frontend/firebase/index.js
@@ -0,0 +1,9 @@
+import * as auth from './auth';
+import { storage }  from './firebase';
+import * as db from './db';
+
+export default {
+  auth,
+  db,
+  storage,
+};
