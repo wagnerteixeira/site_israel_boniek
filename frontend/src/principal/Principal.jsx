a6c477c723dc745dd78ab0db52ed3a0703eb46e9
Wagner Bernardes Teixeira <wagnerbernardesteixeira@gmail.com>
2018-10-16 00:16:08 -0300

    gordinho's request

--- frontend/src/principal/Principal.jsx
+++ frontend/src/principal/Principal.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Principal.css';
 
-import Fundo from '../media/images/fundo_1024.jpg';
+import Fundo from '../media/images/cianotipo.jpg';
 //import Pattern from '../media/images/bg_pattern.png';
 import Nome from '../media/images/nome.svg';
 import withSection from '../hoc/withSection';
@@ -11,7 +11,8 @@ class Principal extends Component {
         return (
             <div id="home" className="container-principal">
                 <img className="image-overlay" src={Nome}/>
-                <div className="image-bg" style={{ backgroundImage: `url(${Fundo})` }} />
+                <img className="image-bg" src={Fundo}/>
+                {/*<div className="image-bg" style={{ backgroundImage: `url(${Fundo})` }} />*/}
             </div>        
         );
     }
