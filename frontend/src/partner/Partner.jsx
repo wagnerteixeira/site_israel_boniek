e37cb4af51b6cfe7b3bec7bae6bfa9781e4e8431
Wagner Bernardes Teixeira <wagnerbernardesteixeira@gmail.com>
2018-12-11 23:20:04 -0200

    change partner image

--- frontend/src/partner/Partner.jsx
+++ frontend/src/partner/Partner.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Partner.css';
 import Text from '../utils/Text';
 
-import Fundo from '../media/images/parceiros.png';
+import Fundo from '../media/images/partner.jpg';
 
 class Partner extends Component {
     render() {
